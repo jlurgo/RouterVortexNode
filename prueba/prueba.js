bbb42e4039bc4976f0ac669d1a124e29722b6da2
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2013-08-04 22:12:31 -0300

    borrados archivos de mas

--- prueba/prueba.js
+++
@@ -1,6 +0,0 @@
-$(function () { 
-    var clienteHTTP = new NodoClienteHTTP('http://localhost:8888', 10);   
-    var portal = new NodoPortalBidi("portal");
-    portal.conectarCon(clienteHTTP);
-    clienteHTTP.conectarCon(portal);
-});
