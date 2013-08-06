8fbe89a31b3104508919aee36ef9d9fd980e1003
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2013-08-06 04:53:14 -0300

    plaplapla

--- web.js
+++ web.js
@@ -16,48 +16,59 @@ var sesiones = [];
 var router = new NodoRouter("principal");
 
 var onRequest = function(request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/html;charset=UTF-8',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods':'GET, POST'        
-    });
     var request_spliteado = request.url.split('/');
     if(request_spliteado.length == 2 && request_spliteado[1] == "create"){
         var sesion = new NodoSesionHttpServer(sesiones.length);
         sesiones.push(sesion);
         router.conectarCon(sesion);
-        sesion.conectarCon(router);
-        response.write(pad(sesiones.length-1, 4));
+        sesion.conectarCon(router);        
         sesion.mensajeParcial = "";
+        response.writeHead(200, {
+            'Content-Type': 'text/html;charset=UTF-8',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods':'GET, POST'        
+        });
+        response.write(pad(sesion.idSesion, 4));
+        response.end();
+        return;
     }    
     if(request_spliteado.length == 3 && request_spliteado[1] == "session"){  
         var nro_sesion = parseInt(request_spliteado[2]);
-        if(nro_sesion<sesiones.length){
-            var sesion = sesiones[nro_sesion];        
-            request.on('data', function (chunk) {
-                sesion.mensajeParcial += chunk.toString();
-              });
-            request.on('end', function () {
-                if(sesion.mensajeParcial!=""){                    
-                    var mensajes_desde_el_cliente = JSON.parse(qs.parse(sesion.mensajeParcial).mensajes_vortex).contenidos;
-                    for(var i=0; i<mensajes_desde_el_cliente.length; i++){
-                        sesion.recibirMensajePorHttp(mensajes_desde_el_cliente[i]);    
-                        if(mensajes_desde_el_cliente[i].tipoDeMensaje == "vortex.video.frame"){
-                            console.log("Recibido un frame de " + mensajes_desde_el_cliente[i].usuarioTransmisor);                            
-                        }
-                    }  
-                    sesion.mensajeParcial = "";
-                }
-                
+        if(nro_sesion>=sesiones.length){
+            res.writeHead(405, "La sesion no existe", {'Content-Type': 'text/html'});
+            response.end();
+        }
+        var sesion = sesiones[nro_sesion];        
+        request.on('data', function (chunk) {
+            sesion.mensajeParcial += chunk.toString();
+          });
+        request.on('end', function () {
+            if(sesion.mensajeParcial!=""){                    
+                var mensajes_desde_el_cliente = JSON.parse(qs.parse(sesion.mensajeParcial).mensajes_vortex).contenidos;
+                for(var i=0; i<mensajes_desde_el_cliente.length; i++){
+                    sesion.recibirMensajePorHttp(mensajes_desde_el_cliente[i]);    
+                    if(mensajes_desde_el_cliente[i].tipoDeMensaje == "vortex.video.frame"){
+                        console.log("Recibido un frame de " + mensajes_desde_el_cliente[i].usuarioTransmisor);                            
+                    }
+                }  
+                sesion.mensajeParcial = "";
+            }
+            var mensajes_para_el_cliente = sesion.getMensajesRecibidos();  
+            response.writeHead(200, {
+                'Content-Type': 'text/html;charset=UTF-8',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods':'GET, POST'        
             });
-            var mensajes_para_el_cliente = sesion.getMensajesRecibidos();    
             response.write(JSON.stringify(
                 {contenidos:mensajes_para_el_cliente,
                  proximaEsperaMinima:0,
                  proximaEsperaMaxima:300000
-                }));  
-        }
+                }));
+            response.end();
+        }); 
+        return;
     }   
+    res.writeHead(405, "Comando no reconocido", {'Content-Type': 'text/html'});
     response.end();
   }
 var puerto = process.env.PORT || 3000;
