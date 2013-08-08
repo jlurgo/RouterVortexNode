5bfb4fd490f0e5d1b4deeac0c310f63308bd4f3b
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2013-08-08 05:25:56 -0300

    bla

--- web.js
+++ web.js
@@ -101,7 +101,6 @@ io.sockets.on('connection', function (socket) {
 io.configure(function () { 
     io.set("transports", ["xhr-polling"]); 
     io.set("polling duration", 10); 
-    io.set("origins", "*");
 });
 
 
