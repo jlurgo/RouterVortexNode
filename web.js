0dcbe6e07bfa555e4364d6bffa3764b57e325b99
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2014-02-25 16:15:22 -0300

    mejorado el server, y actualizado a version 0.0.13 de vortex

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var http = require("http");
 var url = require("url");
-var qs = require('querystring');
 var Vortex = require('vortexjs');
 var express = require('express');
 
-var NodoSesionHttpServer = Vortex.NodoSesionHttpServer;
+var NodoConectorHttpServer = Vortex.NodoConectorHttpServer;
 var NodoRouter = Vortex.NodoRouter;
 var NodoConectorSocket = Vortex.NodoConectorSocket;
 
@@ -16,7 +15,8 @@ var pad = function (n, width, z) {
 
 var sesiones_http = [];
 var sesiones_web_socket = [];
-
+var ultimo_id_sesion_http = 0;
+var ultimo_id_sesion_ws = 0;
 var router = new NodoRouter("principal");
 
 var allowCrossDomain = function(req, res, next) {
@@ -27,75 +27,53 @@ var allowCrossDomain = function(req, res, next) {
 }
 
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 app.use(allowCrossDomain);
 
 app.post('/create', function(request, response){
-    var sesion = new NodoSesionHttpServer(sesiones_http.length);
-    sesiones_http.push(sesion);
-    router.conectarCon(sesion);
-    sesion.conectarCon(router);        
-    sesion.mensajeParcial = "";
-    response.send(pad(sesion.idSesion, 4));
-});
-
-app.post('/session/:nro_sesion', function(request, response){
-    var nro_sesion = parseInt(request.params.nro_sesion);
-    if(nro_sesion>=sesiones_http.length){
-        response.send("La sesión no existe");
-        return;
-    }
-    var sesion = sesiones_http[nro_sesion];        
-    request.on('data', function (chunk) {
-        sesion.mensajeParcial += chunk.toString();
-    });
-    request.on('end', function () {
-        if(sesion.mensajeParcial!=""){                    
-            var mensajes_desde_el_cliente = JSON.parse(qs.parse(sesion.mensajeParcial).mensajes_vortex).contenidos;
-            for(var i=0; i<mensajes_desde_el_cliente.length; i++){
-                sesion.recibirMensajePorHttp(mensajes_desde_el_cliente[i]);    
-                if(mensajes_desde_el_cliente[i].tipoDeMensaje == "vortex.video.frame"){
-                    console.log("Recibido un frame de " + mensajes_desde_el_cliente[i].usuarioTransmisor);                            
-                }
-            }  
-            sesion.mensajeParcial = "";
+    var conector_http = new NodoConectorHttpServer({
+        id: pad(ultimo_id_sesion_http, 4),
+        verbose:true,
+        app: app,
+        alDesconectar: function(){
+            sesiones_http.splice(sesiones_http.indexOf(conector_http), 1);
         }
-        var mensajes_para_el_cliente = sesion.getMensajesRecibidos();  
-        response.send(JSON.stringify(
-            {contenidos:mensajes_para_el_cliente,
-             proximaEsperaMinima:0,
-             proximaEsperaMaxima:300000
-            }));
-    }); 
+    });
+    ultimo_id_sesion_http+=1;
+    sesiones_http.push(conector_http);
+    router.conectarBidireccionalmenteCon(conector_http);     
+    response.send(conector_http.idSesion);
 });
-
-app.get('/infoSesiones', function(request, response){
-    var info_sesiones = {
-        http: sesiones_http.length,
-        webSocket: sesiones_web_socket.length
-    };
-    response.send(JSON.stringify(info_sesiones));
-});
-
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
-    console.log("nueva conexion socket");
-    var sesion_socket = new NodoConectorSocket(socket);
-    router.conectarBidireccionalmenteCon(sesion_socket);
-    sesiones_web_socket.push(sesion_socket);
-    socket.on('disconnect', function () {
-        var index = sesiones_web_socket.indexOf(sesion_socket);
-        console.log('socket ' + index + ' desconectado');
-        sesiones_web_socket.splice(index, 1);        
+    var conector_socket = new NodoConectorSocket({
+        id: ultimo_id_sesion_ws.toString(),
+        socket: socket, 
+        verbose: true, 
+        alDesconectar:function(){
+            sesiones_web_socket.splice(sesiones_web_socket.indexOf(conector_socket), 1);
+        }
     });
+    ultimo_id_sesion_ws+=1;
+    sesiones_web_socket.push(conector_socket);
+    router.conectarBidireccionalmenteCon(conector_socket);
 });
 
 io.configure(function () { 
     io.set("transports", ['websocket', 'flashsocket', 'xhr-polling']); 
     io.set("polling duration", 10); 
     io.disable('log');
+});
+
+app.get('/infoSesiones', function(request, response){
+    var info_sesiones = {
+        http: sesiones_http.length,
+        webSocket: sesiones_web_socket.length,
+        router: router._patas.length
+    };
+    response.send(JSON.stringify(info_sesiones));
 });
 
 var puerto = process.env.PORT || 3000;
